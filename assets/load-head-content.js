1b827139fbf33914ce879ac8938a75a97ba45a55
liaochn <65910889+liaochn@users.noreply.github.com>
2025-03-10 15:09:41 +0800

    V.3.3

--- assets/load-head-content.js
+++ assets/load-head-content.js
@@ -3,6 +3,6 @@ fetch('./assets/head-content.html')
     .then(response => response.text())
     .then(data => {
         // 将内容插入到 <head> 中
-        document.head.insertAdjacentHTML('before end', data);
+        document.head.insertAdjacentHTML('beforeend', data);
     })
     .catch(error => console.error('Fail to load head-content.html', error));
